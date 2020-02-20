ad5beef15a28d684e606e79d82406afeb6f8cf10
gitkiselev <482003@mail.ru>
2020-02-20 12:46:05 +0300

    HW3 Yes or No checkbox content

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+const DAY_STRING = ['день', 'дня', 'дней'];
+
 const DATA = {
     whichSite: ['landing', 'multiPage', 'onlineStore'],
     price: [4000, 8000, 26000],
@@ -19,15 +21,34 @@ const startButton = document.querySelector('.start-button'),
       total = document.querySelector('.total'),
       fastRange = document.querySelector('.fast-range'),
       totalPriceSum = document.querySelector('.total_price__sum'),
-      adapt = document.querySelector('#adapt'),
-      mobileTemplates = document.querySelector('#mobileTemplates');
-
-      mobileTemplates.readOnly = true;
+      desktopTemplates = document.getElementById('desktopTemplates'),
+      adapt = document.getElementById('adapt'),
+      mobileTemplates = document.getElementById('mobileTemplates'),
+      editable = document.getElementById('editable'),
+      typeSite = document.querySelector('.type-site'),
+      maxDeadline = document.querySelector('.max-deadline'),
+      rangeDeadline = document.querySelector('.range-deadline'),
+      deadlineValue = document.querySelector('.deadline-value');
+      //checkboxLabel = document.querySelector('.deadline-value');
       
 
+function declOfNum(n, titles) {
+    return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+                            0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
 
-
-
+function checkboxContent () {
+    const answers = [desktopTemplates, adapt, mobileTemplates, editable];
+    for(const elem of answers) {
+            let content = elem.nextElementSibling.nextElementSibling;
+            //console.log('Элемент контента: ' + content)
+                if(elem.checked) {
+                    content.textContent = 'Да'
+                } else {
+                    content.textContent = 'Нет';
+                }
+    }
+}
 function showElem(elem) {
     elem.style.display = 'block';
 }
@@ -36,11 +57,23 @@ function hideElem(elem) {
     elem.style.display = 'none';
 }
 
+function renderTextContent (total, site, maxDay, minDay) {
+    totalPriceSum.textContent = total;
+    typeSite.textContent = site;
+    maxDeadline.textContent = declOfNum(maxDay, DAY_STRING);
+    rangeDeadline.min = minDay;
+    rangeDeadline.max = maxDay;
+    deadlineValue.textContent = declOfNum(rangeDeadline.value, DAY_STRING)
+}
 function priceCalculation(elem) {
-    needAdapt();
+    //needAdapt();
+    checkboxContent();
     let result = 0,
         index = 0,
-        options = [];
+        options = [],
+        site = '',
+        maxDeadlineDay = DATA.deadlineDay[index][1],
+        minDeadlineDay = DATA.deadlineDay[index][0];
 
 
     if (elem.name === 'whichSite') {
@@ -54,8 +87,10 @@ function priceCalculation(elem) {
 
     for (const item of formCalculate.elements) {
         if (item.name === 'whichSite' && item.checked) {
-            //console.clear()
             index = DATA.whichSite.indexOf(item.value);
+            site = item.dataset.site;
+            maxDeadlineDay = DATA.deadlineDay[index][1];
+            minDeadlineDay = DATA.deadlineDay[index][0];
         } else if (item.classList.contains('calc-handler') && item.checked) {
             options.push(item.value)
         }
@@ -78,21 +113,26 @@ function priceCalculation(elem) {
     })
 
     result += DATA.price[index];
-
-    totalPriceSum.textContent = result;
+    renderTextContent(result, site, maxDeadlineDay, minDeadlineDay);
 }
 
-function needAdapt () {
-    if (!adapt.checked) {
-        //console.log('checked')
-        mobileTemplates.checked = false;
-        mobileTemplates.readOnly = true;
-    } else {
-        mobileTemplates.readOnly = false;
-    }
-}
+// function needAdapt () {
+//     if (!adapt.checked) {
+//         mobileTemplates.checked = false;
+//         mobileTemplates.readOnly = true;
+//     } else {
+//         mobileTemplates.readOnly = false;
+//     }
+// }
 function handlerCallBackForm(e) {
     const target = e.target;
+
+    if (adapt.checked) {
+        mobileTemplates.disabled = false;
+    } else {
+        mobileTemplates.disabled = true;
+        mobileTemplates.checked = false;
+    }
     if (target.classList.contains('want-faster')) {  
         target.checked ? showElem(fastRange) : hideElem(fastRange);
     }
